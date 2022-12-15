0a46e3ad7b01fb9d299c30e93f6e25b55a254b60
MiloUros <urosmilovanovic955@gmail.com>
2022-12-15 08:41:38 +0100

    feat - adding app info for swagger.

--- src/main/java/com/ingsoftware/contactmanager/config/SwaggerConfig.java
+++ src/main/java/com/ingsoftware/contactmanager/config/SwaggerConfig.java
@@ -6,9 +6,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
 
 @Configuration
 @EnableSwagger2
@@ -20,7 +24,19 @@ public class SwaggerConfig {
                 .select()
                 .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
                 .build()
-                .directModelSubstitute(Pageable.class, SwaggerPageable.class);
+                .directModelSubstitute(Pageable.class, SwaggerPageable.class)
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo("Contact Manager",
+                "ING intern assignment",
+                "1.0",
+                "Terms of service",
+                new Contact("Uros Milovanovic", "", "urosmilovanovic955@gmail.com"),
+                "License of API",
+                "API license URL",
+                Collections.emptyList());
     }
 
 }
